b32bd8e47cee6e6b05dd0d5b55f2dfbfc5e6fecd
Zeynep Inkaya <56036659+ZeySN@users.noreply.github.com>
2020-05-20 14:51:25 +0300

    uploaded files

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
+/*
+zeynep inkaya
+zeynsemn@gmail.com
+https://cloudflare-2020-int.zeynsemn.workers.dev/
+*/
+
 addEventListener('fetch', event => {
+  
   event.respondWith(handleRequest(event.request))
+  
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+// /**
+//  * Respond with hello worker text
+//  * @param {Request} request
+//  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+    //response1 is json of 'https://cfw-takehome.developers.workers.dev/api/variants' fetch
+    let response1 = await getRequest('https://cfw-takehome.developers.workers.dev/api/variants')
+    if(response1 === "error"){
+        return new Response("error", {
+            headers: { 'content-type': 'text/plain' },
+        })
+    }
+    let obj = JSON.parse(JSON.stringify(response1))
+    let random_number = generateRandomNumber()
+    //generateRandomNumber returns either 0 or 1, then get response2 from variants
+    let response2 = getRequest(JSON.stringify(obj.variants[random_number]))
+    let redirect_url = new URL(obj.variants[random_number])
+    console.log(redirect_url)
+    //redirect to the variants1 or variants2
+    return Response.redirect(redirect_url)
+    
+    
+    
+}
+//return the json of fetch request
+async function getRequest(url){
+    
+    let response = await fetch(url)
+    if( !(response.status >= 200) && !(response.status < 300)){
+        return "error"
+    }
+    let data = await response.json()
+    .catch(err=> err)
+    return data
+}
+
+function generateRandomNumber(){
+    return (Math.floor(Math.random() * 11) % 2)
 }
